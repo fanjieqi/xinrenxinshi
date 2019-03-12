0b6a85faf96a78a5c2031c4a73f71427a2ff2bcc
fanjieqi <fanjieqi@nibirutech.com>
2019-03-12 10:08:14 +0800

    Initial commit

---
+++ lib/xinrenxinshi.rb
@@ -0,0 +1,5 @@
+require "xinrenxinshi/version"
+
+module Xinrenxinshi
+  # Your code goes here...
+end
---
+++ spec/xinrenxinshi_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Xinrenxinshi do
+  it "has a version number" do
+    expect(Xinrenxinshi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
